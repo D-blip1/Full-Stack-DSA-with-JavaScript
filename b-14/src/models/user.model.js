dfbf49a38bd542b11867e43af2c21727444a5524
lowXenergy <divyajyoti.mailbox@gmail.com>
2026-08-25 19:17:31 +0530

    Indexing and stuff

--- b-14/src/models/user.model.js
+++ b-14/src/models/user.model.js
@@ -6,6 +6,12 @@ const userSchema = new mongoose.Schema({
     age:Number
 });
 
+userSchema.index({email:1}); // This will create an index on the email field, which will make the query faster when searching for a user by email. The 1 indicates that the index should be created in ascending order. You can also use -1 for descending order.
+
+userSchema.index({ username: 1, age: -1 }); // This will create a compound index on the username and age fields, which will make the query faster when searching for a user by username and age. The 1 indicates that the index should be created in ascending order for username, and -1 indicates that the index should be created in descending order for age.
+
+// userSchema.index({ email: 1 }, { unique: true });// This will create a unique index on the email field, which will ensure that no two users can have the same email address. If you try to create a user with an email that already exists in the database, it will throw an error.
+
 const userModel = mongoose.model("user",userSchema);
 
 module.exports = userModel
--- b-14/src/routes/auth.routes.js
+++ b-14/src/routes/auth.routes.js
@@ -19,4 +19,54 @@ authRouter.post("/register",async(req,res)=>{
     })
 })
 
+authRouter.get("/find",async(req,res)=>{
+    const {email}=req.body
+    // const email = req.params.email // Tried it and i need to change the route to /find/:email to use this
+    // const email = req.query // Tried it and i need to change the route to /
+
+    console.log(email)
+    const user = await userModel.findOne({email}).explain("executionStats");// This will give you the execution stats of the query, which can help you understand how the query is being executed and if there are any performance issues. It will give "stage": "COLLSCAN" when the query is not using an index and is scanning the entire collection. If you see this, it means that the query is not optimized and you should consider adding an index on the email field.
+
+    console.log(JSON.stringify(user, null, 2));// stringify console output readable format. null → no custom replacer 2    → indentation of 2 spaces. JSON.stringify() is not required for explain() to work.
+
+    if(!user){
+        return res.status(404).json({
+            message:"User not found"
+        })
+    }
+
+    res.status(200).json({
+        message:"User found successfully",
+        user
+    })
+})
+
+authRouter.get("/find-user/:username/:age", async (req, res) => {
+    const { username, age } = req.params;
+
+    const user = await userModel
+        .findOne({
+            username,
+            age: Number(age)
+        })
+        .explain("executionStats");
+
+
+    res.status(200).json({
+        message: "Execution plan",
+        user
+    });
+});
+
+// Test compound index order
+authRouter.get("/find-username/:username", async (req, res) => {
+    const { username } = req.params;
+
+    const result = await userModel
+        .find({ username })
+        .explain("executionStats");
+
+    res.json(result);
+});
+
 module.exports = authRouter
